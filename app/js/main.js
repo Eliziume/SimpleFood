ffb0846eb54c221db77ea1d947b0678fdc2b461c
Eliziume <Volchok2000@mail.ru>
2023-05-11 18:43:00 +0300

    подправленное 16 дз

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,56 @@
 $(function () {
+
+  class ItcTabs {
+    constructor(target, config) {
+      const defaultConfig = {};
+      this._config = Object.assign(defaultConfig, config);
+      this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
+      this._elButtons = this._elTabs.querySelectorAll('.tabs__btn');
+      this._elPanes = this._elTabs.querySelectorAll('.tabs__pane');
+      this._eventShow = new Event('tab.itc.change');
+      this._init();
+      this._events();
+    }
+    _init() {
+      this._elTabs.setAttribute('role', 'tablist');
+      this._elButtons.forEach((el, index) => {
+        el.dataset.index = index;
+        el.setAttribute('role', 'tab');
+        this._elPanes[index].setAttribute('role', 'tabpanel');
+      });
+    }
+    show(elLinkTarget) {
+      const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+      const elLinkActive = this._elTabs.querySelector('.tabs__btn--active');
+      const elPaneShow = this._elTabs.querySelector('.tabs__pane--show');
+      if (elLinkTarget === elLinkActive) {
+        return;
+      }
+      elLinkActive ? elLinkActive.classList.remove('tabs__btn--active') : null;
+      elPaneShow ? elPaneShow.classList.remove('tabs__pane--show') : null;
+      elLinkTarget.classList.add('tabs__btn--active');
+      elPaneTarget.classList.add('tabs__pane--show');
+      this._elTabs.dispatchEvent(this._eventShow);
+      elLinkTarget.focus();
+    }
+    showByIndex(index) {
+      const elLinkTarget = this._elButtons[index];
+      elLinkTarget ? this.show(elLinkTarget) : null;
+    };
+    _events() {
+      this._elTabs.addEventListener('click', (e) => {
+        const target = e.target.closest('.tabs__btn');
+        if (target) {
+          e.preventDefault();
+          this.show(target);
+        }
+      });
+    }
+  }
+
+  // инициализация .tabs как табов
+  new ItcTabs('.tabs');
+
   $(".rating").rateYo({
     readOnly: true,
     starWidth: "16px",
@@ -113,21 +165,21 @@ $(function () {
         }
       },
       {
-        breakpoint: 992,
+        breakpoint: 993,
         settings: {
           slidesToShow: 3
         }
       },
 
       {
-        breakpoint: 768,
+        breakpoint: 769,
         settings: {
           slidesToShow: 2
         }
       },
 
       {
-        breakpoint: 560,
+        breakpoint: 561,
         settings: {
           slidesToShow: 2,
           dots: true,
